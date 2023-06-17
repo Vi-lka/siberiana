e9d574069570ecaf3fcb363da4eb4d7f5e9ac8a4
Dmtitry Krasnov <dkrasnovdev@gmail.com>
2023-06-17 13:07:33 +0700

    (feat): dotenv-cli, global .env

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center">
       <h1 className="text-beaver w-fit">@siberiana/client</h1>
-
       <Button className="bg-beaverLight hover:bg-beaver text-graphite hover:text-beaverLight mt-6 w-fit rounded-full uppercase">
         Button
       </Button>
