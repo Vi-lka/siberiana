b0b9e7663f33105a5b3997284d6a9edc8b7ee1dc
vi <vitalya.permyakov155@gmail.com>
2023-08-28 10:24:32 +0700

    feat(client): no atom with storage, its just not needed

--- apps/client/src/lib/utils/atoms.ts
+++ apps/client/src/lib/utils/atoms.ts
@@ -1,10 +1,9 @@
 import "client-only"
 
 import { atom } from 'jotai'
-import { atomWithStorage } from 'jotai/utils'
 
-export const tabAccountAtom = atomWithStorage('tab', "favourites")
-export const tabObjectsAtom = atomWithStorage("tabObject", "artifacts")
+export const tabAccountAtom = atom("favourites")
+export const tabObjectsAtom = atom("artifacts")
 
 export const artifactsCountAtom = atom(0) 
 export const booksCountAtom = atom(0)
