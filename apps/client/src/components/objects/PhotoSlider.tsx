aa43e99e72f13f3d221f52383952a0ffa6e6f9d7
vi <vitalya.permyakov155@gmail.com>
2023-09-04 21:40:39 +0700

    feat(client): photo slider 'dots' control

--- apps/client/src/components/objects/PhotoSlider.tsx
+++ apps/client/src/components/objects/PhotoSlider.tsx
@@ -1,14 +1,12 @@
 "use client"
 
-import React from 'react'
-
 import "keen-slider/keen-slider.min.css";
 
-import type { TrackDetails} from "keen-slider/react";
+import React from 'react'
 import { useKeenSlider } from "keen-slider/react";
-
 import { Skeleton } from "@siberiana/ui";
 import PhotoSliderImg from './PhotoSliderImg';
+import { cn } from "@siberiana/ui/src/lib/utils";
 
 export default function PhotoSlider({ 
     data 
@@ -19,6 +17,7 @@ export default function PhotoSlider({
     }[] 
 }) {
     const [created, setCreated] = React.useState<boolean>();
+    const [currentSlide, setCurrentSlide] = React.useState(0)
 
     const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
       loop: true,
@@ -28,6 +27,9 @@ export default function PhotoSlider({
         origin: "center",
         spacing: typeof window !== "undefined" && window.innerWidth <= 740 ? 3 : 6,
       },
+      slideChanged(slider) {
+        setCurrentSlide(slider.track.details.rel)
+      },
       created() {
         setCreated(true);
       },
@@ -36,25 +38,54 @@ export default function PhotoSlider({
       },
     });
 
+    console.log(currentSlide)
 
   return (
-    <div ref={sliderRef} className="keen-slider cursor-grab max-h-96 md:aspect-video aspect-square rounded-md bg-accent">
-        {data.map((image, index) => (
-            <div
-              key={index}
-              className="keen-slider__slide zoom-out__slide"
-            >
-                {created ? (
-                    
-                  <PhotoSliderImg
-                    src={image.src}
-                    alt={image.alt}
-                  />
-                ) : (
-                  <Skeleton className="h-full w-full" />
-                )}
+    <div className="">
+      <div ref={sliderRef} className="keen-slider cursor-grab max-h-96 md:aspect-video aspect-square rounded-md bg-accent">
+          {data.map((image, index) => (
+              <div
+                key={index}
+                className="keen-slider__slide zoom-out__slide"
+              >
+                  {created ? (
+
+                    <PhotoSliderImg
+                      src={image.src}
+                      alt={image.alt}
+                    />
+                  ) : (
+                    <Skeleton className="h-full w-full" />
+                  )}
+              </div>
+          ))}
+      </div>
+      {(created && !!instanceRef.current && data.length > 1) 
+        ? (
+          <div className="flex items-center gap-3 mt-2">
+            <div className="flex gap-2 flex-auto">
+              {data.map((_, index) =>  (
+                  <button
+                    key={index}
+                    onClick={() => {
+                      instanceRef.current?.moveToIdx(index)
+                    }}
+                    className="flex-auto py-4"
+                  >
+                    <div 
+                      className={cn(
+                        "h-0.5 w-full transition-all",
+                        currentSlide === index ? "bg-input" : "bg-accent"
+                      )} 
+                    />
+                  </button>
+                )
+              )}
             </div>
-        ))}
+          </div>
+        )
+        : <Skeleton className="h-2 w-full mt-6" />
+      }
     </div>
   )
 }
