1c6d7c7e0678a811b36e5dca25abb763809da859
Vi-lka <vitalya.permyakov155@gmail.com>
2023-11-04 15:03:49 +0700

    start auth branch

--- apps/client/src/app/api/auth/[...nextauth]/route.ts
+++ apps/client/src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,4 @@
 /* eslint-disable @typescript-eslint/require-await */
-/* eslint-disable @typescript-eslint/restrict-plus-operands */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
