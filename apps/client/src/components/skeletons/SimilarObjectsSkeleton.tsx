a49002c896bb0c8002bf8c863e3b884bb3f7dc21
PetyaOgurkin <flash550@yandex.ru>
2023-12-13 00:11:29 +0700

    feat: added title to skeleton, fix images

--- apps/client/src/components/skeletons/SimilarObjectsSkeleton.tsx
+++ apps/client/src/components/skeletons/SimilarObjectsSkeleton.tsx
@@ -2,10 +2,14 @@ import { Skeleton } from "@siberiana/ui";
 
 export default function SimilarObjectsSkeleton() {
   return (
-    <div className="flex flex-col gap-10 md:grid md:grid-cols-4 md:gap-4 xl:gap-10">
-      {Array(4).map((_, i) => (
+    <div className="mb-20">
+      <Skeleton className="mb-10 h-8 w-80" />
+      <div className="flex flex-col gap-10 md:grid md:grid-cols-4 md:gap-4 xl:gap-10">
         <Skeleton className="aspect-square w-full" />
-      ))}
+        <Skeleton className="aspect-square w-full" />
+        <Skeleton className="aspect-square w-full" />
+        <Skeleton className="aspect-square w-full" />
+      </div>
     </div>
   );
 }
